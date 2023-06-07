5652de0b4ccecb92486ded948d538d32a0a2f8f6
Vlados <vladishchenko65@gmail.com>
2023-06-07 22:23:33 +0500

    poka

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main()
 {
+	int x, y, SUMMA;
+
+	cout << "Enter x: " << endl; cin >> x;
+	cout << "Enter y: " << endl; cin >> y;
+	cout << "SUMMA: " << x + y << endl; 
 	return 0;
 }
